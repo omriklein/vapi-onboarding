e11b9507ddbf2cbda0200b9beea98e65f6d341df
omriklein <omrik2007@gmail.com>
2025-09-02 12:38:14 +0300

    [e2e] addd email verify test

---
+++ e2e/cypress/tests/verifyEmail.spec.ts
@@ -0,0 +1,22 @@
+describe("Verifty got email today", () => {
+  beforeEach(() => {
+    // Ignore any uncaught exceptions on the mailinator page
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
+    });
+  });
+
+  it("should complete signup and agent setup", () => {
+
+    cy.visit("https://www.mailinator.com/");
+
+    cy.contains("Public Inbox").should("be.visible");
+
+    const email = "vapitest@mailinator.com"; // TODO: Change to take from env file
+    cy.get('input#search').type(email);
+    cy.contains("GO").click();
+
+    cy.contains("VAPI Obnoarding").click();
+    cy.contains("VAPI Onboarding – Last 24 Hours").should("be.visible");
+  });
+});
